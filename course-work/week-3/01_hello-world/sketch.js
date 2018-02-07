1d50bdb76361951def7437daf99569711a58769b
lindsayelaird <lindsayelaird@users.noreply.github.com>
2018-02-07 15:56:13 -0700

    Hw part 1

---
+++ course-work/week-3/01_hello-world/sketch.js
@@ -0,0 +1,3 @@
+function setup(){
+    ellipse(80, 80, 80, 50);
+}
---
+++ hw-3/sketch.js
@@ -0,0 +1,16 @@
+// Declare the setup function
+function setup(){
+
+    // create a canvas
+    createCanvas( 600, 400 );
+
+    // color the background 'cyan'
+    background( 'cyan' );
+}
+// Declare the draw function
+function draw(){
+
+    // tell p5 to print the text string "Hello World!"
+    // this is going to be positioned at ( x: 20, y: 30).
+    text( "Hello World!", 20, 30 );
+}
